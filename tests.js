6f008caea9b9b2d087f4d1ea11a55db0d36e72b2
Taylor Andersen <tay.andersen99@gmail.com>
2023-10-18 11:47:23 -0500

    completed Exercise #11 "isEven" function

--- tests.js
+++ tests.js
@@ -48,3 +48,35 @@ describe('isFive', function() {
         expect(isFive('5')).toBe(true);
     })
 })
+describe('isEven', function() {
+    it('should be a function named "isEven"', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean no matter what', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('returns true when executed with isEven(2)', function() {
+        expect(isEven(2)).toBe(true);
+    });
+    it('returns true when executed with isEven(-4)', function() {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('returns false when executed with isEven(3)', function() {
+        expect(isEven(3)).toBe(false);
+    });
+    it('returns false when executed with isEven("banana")', function() {
+        expect(isEven('banana')).toBe(false);
+    });
+    it('returns true when executed with isEven("8")', function() {
+        expect(isEven("8")).toBe(true);
+    });
+    it('returns false when executed with isEven(Infinity)', function() {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('returns false when executed with a boolean input', function() {
+        expect(isEven(true)).toBe(false) && expect(isEven(false)).toBe(false);
+    });
+    it('returns false when executed without an argument like isEven()', function() {
+        expect(isEven()).toBe(false);
+    });
+})
